03ad485b4fc9b685cf6aabfa49de9e5f52d63394
Vinayaka Jyothi <vj338@nyu.edu>
2020-05-02 03:34:46 +0530

    Update modelstats.py

--- medicalai/chief/model_metrics/modelstats.py
+++ medicalai/chief/model_metrics/modelstats.py
@@ -15,8 +15,12 @@
 from __future__ import absolute_import
 import numpy as np
 import pandas as pd
-from sklearn.metrics import average_precision_score,precision_recall_curve,roc_auc_score,roc_curve,f1_score
+from sklearn.metrics import average_precision_score,precision_recall_curve,roc_auc_score,roc_curve,f1_score,confusion_matrix
+import seaborn as sns
 import matplotlib.pyplot as plt
+sns.set(style='darkgrid',palette="muted", color_codes=True)
+import six
+from matplotlib.backends.backend_pdf import PdfPages
 def true_positives(expected, preds, threshold=0.5):
     """
     Count true positives.
@@ -210,6 +214,7 @@ def get_weighted_loss(pos_weights, neg_weights, epsilon=1e-7):
     Returns:
       weighted_loss (function): weighted loss function
     """
+    from tensorflow.keras import backend as K
     def weighted_loss(y_true, y_pred):
         """
         Return weighted loss value. 
@@ -226,9 +231,60 @@ def get_weighted_loss(pos_weights, neg_weights, epsilon=1e-7):
         return loss 
     return weighted_loss
 
+def get_curve(gt, pred, target_names, curve='roc',returnPlot = False, showPlot= True, axes=None, **kwargs):
+    for i in range(len(target_names)):
+        if curve == 'roc':
+            curve_function = roc_curve
+            auc_roc = roc_auc_score(gt[:, i], pred[:, i])
+            label = target_names[i] + " AUC: %.3f " % auc_roc
+            a, b, _ = curve_function(gt[:, i], pred[:, i])
+            if showPlot:
+                fig = plt.figure(2, figsize=(10, 10))
+                ax = plt
+                ax.xlabel("False positive rate")
+                ax.ylabel("True positive rate")
+                ax.title('ROC Curve')
+            else:
+                ax = axes
+                ax.set_xlabel('False positive rate')
+                ax.set_ylabel('True positive rate')
+                ax.set_title('ROC Curve')                
+            plt.plot([0, 1], [0, 1], 'k--')
+            plt.plot(a, b, label=label)
+        elif curve == 'precision_recall_curve':
+            precision, recall, _ = precision_recall_curve(gt[:, i], pred[:, i])
+            average_precision = average_precision_score(gt[:, i], pred[:, i])
+            label = target_names[i] + " Avg.: %.3f " % average_precision
+            if showPlot:
+                fig = plt.figure(2, figsize=(10, 10))
+                ax = plt
+                ax.xlabel('Recall')
+                ax.ylabel('Precision')
+                ax.title('Precision Recall Curve',fontsize = 18, color='r')
+                ax.ylim([0.0, 1.05])
+                ax.xlim([0.0, 1.0])
+            else:
+                ax = axes
+                ax.set_xlabel('Recall')
+                ax.set_ylabel('Precision')
+                ax.set_title('Precision Recall Curve',fontsize = 18, color='r')  
+                ax.set_ylim([0.0, 1.05])
+                ax.set_xlim([0.0, 1.0])
+            plt.step(recall, precision, where='post', label=label)
+
+        plt.legend(loc='best')
+    #print('showPlot ',showPlot)
+    if showPlot==True:
+        plt.show()
+        if returnPlot==True:
+            return fig
+    else:
+        if returnPlot==True:
+            return ax
 
 
-def get_roc_curve(labels, predicted_vals, groundTruth= None, generator=None , returnPlot = False, showPlot= True):
+
+def get_roc_curve(labels, predicted_vals, groundTruth= None, generator=None , returnPlot = False, showPlot= True, axes=None, **kwargs):
     auc_roc_vals = []
     for i in range(len(labels)):
         try:
@@ -242,25 +298,37 @@ def get_roc_curve(labels, predicted_vals, groundTruth= None, generator=None , re
             auc_roc = roc_auc_score(gt, pred)
             auc_roc_vals.append(auc_roc)
             fpr_rf, tpr_rf, _ = roc_curve(gt, pred)
-            plt.figure(1, figsize=(10, 10))
-            plt.plot([0, 1], [0, 1], 'k--')
-            plt.plot(fpr_rf, tpr_rf,
-                     label=labels[i] + " (" + str(round(auc_roc, 3)) + ")")
-            plt.xlabel('False positive rate')
-            plt.ylabel('True positive rate')
-            plt.title('ROC curve')
-            plt.legend(loc='best')
-        except:
-            print(
-                "Error in generating ROC curve for",labels[i],
-                "\nDataset lacks enough examples."
-            )
+            if showPlot:
+                fig = plt.figure(1,figsize=(10, 10))
+                ax = plt
+                ax.xlabel('False positive rate')
+                ax.ylabel('True positive rate')
+                ax.title('ROC Curve',fontsize = 18, color='r')
+            else:
+                ax = axes
+                ax.set_xlabel('False positive rate')
+                ax.set_ylabel('True positive rate')
+                ax.set_title('ROC Curve',fontsize = 18, color='r')
+            ax.plot([0, 1], [0, 1], 'k--')
+            ax.plot(fpr_rf, tpr_rf,
+                        label=labels[i] + " (" + str(round(auc_roc, 3)) + ")")
+
+            
+            ax.legend(loc='best')
+        except Exception as err:
+            print("Error in generating ROC curve for",labels[i], '\n',err)
+    #print('showPlot ROC Curve',showPlot)
     if showPlot==True:
         plt.show()
-    if returnPlot==True:
-        return auc_roc_vals, plt
+        if returnPlot==True:
+            return auc_roc_vals, fig
+        else:
+            return auc_roc_vals
     else:
-        return auc_roc_vals
+        if returnPlot==True:
+            return auc_roc_vals, ax
+        else:
+            return auc_roc_vals        
 
 
 def bootstrap_auc(y, pred, classes, bootstraps = 100, fold_size = 1000):
@@ -370,39 +438,6 @@ def confidence_intervals(class_labels, statistics):
     return df
 
 
-def get_curve(gt, pred, target_names, curve='roc',returnPlot = False, showPlot= True):
-    for i in range(len(target_names)):
-        if curve == 'roc':
-            curve_function = roc_curve
-            auc_roc = roc_auc_score(gt[:, i], pred[:, i])
-            label = target_names[i] + " AUC: %.3f " % auc_roc
-            xlabel = "False positive rate"
-            ylabel = "True positive rate"
-            a, b, _ = curve_function(gt[:, i], pred[:, i])
-            plt.figure(1, figsize=(7, 7))
-            plt.plot([0, 1], [0, 1], 'k--')
-            plt.plot(a, b, label=label)
-            plt.xlabel(xlabel)
-            plt.ylabel(ylabel)
-
-            plt.legend(loc='upper center', bbox_to_anchor=(1.3, 1),
-                       fancybox=True, ncol=1)
-        elif curve == 'precision_recall_curve':
-            precision, recall, _ = precision_recall_curve(gt[:, i], pred[:, i])
-            average_precision = average_precision_score(gt[:, i], pred[:, i])
-            label = target_names[i] + " Avg.: %.3f " % average_precision
-            plt.figure(1, figsize=(7, 7))
-            plt.step(recall, precision, where='post', label=label)
-            plt.xlabel('Recall')
-            plt.ylabel('Precision')
-            plt.ylim([0.0, 1.05])
-            plt.xlim([0.0, 1.0])
-            plt.legend(loc='upper center', bbox_to_anchor=(1.3, 1),
-                       fancybox=True, ncol=1)
-    if showPlot==True:
-        plt.show()
-    if returnPlot==True:
-        return plt
 
 from sklearn.calibration import calibration_curve
 def plot_calibration_curve(y, pred,class_labels):
@@ -433,23 +468,178 @@ def platt_scaling(y, pred,class_labels):
     return pred_calibrated
 
 
-def get_detailed_evaluation(CLASS_NAMES, predictions, groundTruth=None,  generator=None ,):
-    gt_one_hot_vec = np.identity(OUTPUT_CLASSES)[groundTruth[:,0]]
+def generate_evaluation_report(CLASS_NAMES, predictions, groundTruth=None,  generator=None, returnPlot = True, showPlot= True , printStat=True, **kwargs):
     OUTPUT_CLASSES = len(CLASS_NAMES)
     if groundTruth is not None and generator is None:
-        auc_roc_vals, roc_plt = get_roc_curve(CLASS_NAMES,predictions,groundTruth=gt_one_hot_vec, returnPlot = True, showPlot= True)
+        gt_one_hot_vec = np.identity(OUTPUT_CLASSES)[groundTruth[:,0]]
+        gt = groundTruth[:,0]
     elif generator is not None:
-        auc_roc_vals, roc_plt = get_roc_curve(CLASS_NAMES,predictions,generator=generator, returnPlot = True, showPlot= True)
+        gt_one_hot_vec = np.identity(OUTPUT_CLASSES)[generator.labels]
+        gt = generator.labels
 
+    if not showPlot:
+        nrows=kwargs['nrows'] if 'nrows' in kwargs else 2
+        ncols=kwargs['ncols'] if 'ncols' in kwargs else 1
+        pad = kwargs['pad'] if 'pad' in kwargs else 10
+        hspace = kwargs['hspace'] if 'hspace' in kwargs else 0.3
+        hspace2 = kwargs['hspace2'] if 'hspace2' in kwargs else 0.3
+        figSize = kwargs['figsize'] if 'figsize' in kwargs else (10,10)
+        modelName = kwargs['modelName'] if 'modelName' in kwargs else 'Model Name'
+        pdfName = kwargs['pdfName'] if 'pdfName' in kwargs and kwargs['pdfName'] is not None else modelName
+        fig = plt.figure(figsize=figSize)    
+        axs = fig.subplots(nrows=nrows, ncols=ncols)
+        fig.tight_layout(pad=pad)
+        plt.subplots_adjust(hspace=hspace)
+    else:
+        axs = [None for x in range(0,nrows*ncols)]
+    auc_roc_vals, roc_plt = get_roc_curve(CLASS_NAMES,predictions,groundTruth=gt_one_hot_vec, generator=None,
+                                            returnPlot = True, showPlot= showPlot, axes=axs[0])
     prefMetrics = get_performance_metrics(gt_one_hot_vec, predictions, CLASS_NAMES, acc=get_accuracy, prevalence=get_prevalence, 
                             sens=get_sensitivity, spec=get_specificity, ppv=get_ppv, npv=get_npv, auc=roc_auc_score, f1=f1_score)
-    print(prefMetrics)
+    
     statistics = bootstrap_auc(gt_one_hot_vec, predictions, CLASS_NAMES)
     confInterval = confidence_intervals(CLASS_NAMES, statistics)
-    print(confInterval)
 
-    prc_plt = get_curve(gt_one_hot_vec, predictions, CLASS_NAMES, curve='precision_recall_curve', returnPlot = False, showPlot= True)
 
-    plots =[roc_plt, prc_plt]
+    prc_plt = get_curve(gt_one_hot_vec, predictions, CLASS_NAMES, curve='precision_recall_curve', returnPlot = False, showPlot= showPlot, axes=axs[1])
+
+    
+    dfSpec = prefMetrics[['Sensitivity','Specificity', 'PPV', 'NPV', 'AUC', 'F1']]#.astype(float)
+    dfAcc= prefMetrics[['TP', 'TN', 'FP', 'FN', 'Accuracy', 'Prevalence']]
+
+    fig2 = plt.figure(figsize=(15,15))
+    fig2.suptitle("AI-Bharata MedicalAI - Model Evaluation Report Generator\n\nModel Name : {:}\n".format(modelName.replace('\\','/').split('/')[-1]), horizontalalignment='center', fontsize=18,color='grey') 
+    axs2 = fig2.subplots(nrows=3, ncols=1)
+    axs2[0] = render_df_as_table(dfSpec,  ax= axs2[0], title = 'Model Sensitivity and Specificity Details (Th={})'.format(prefMetrics['Threshold'][0]), header_columns=0, col_width=2.2, resetIndex=True)
+    axs2[1] = render_df_as_table(dfAcc, ax= axs2[1], title = 'Validation Accuracy Details (Th={})'.format(prefMetrics['Threshold'][0]),header_columns=0, col_width=2.2, header_color='#DE2E81', resetIndex=True)
+    axs2[2] = render_df_as_table(confInterval, ax= axs2[2],title = 'Confidence Interval',header_columns=0, col_width=4, header_color='#2BC4C5', resetIndex=True)
+
+    fig3 = plt.figure(figsize=(10,10))
+    axs3 = fig3.subplots(nrows=1, ncols=1)
+    con_mat_norm_df, con_mat_df,Accuracy,cohenKappaScore =  _CM_calculate(np.argmax(predictions, axis=-1),gt,CLASS_NAMES)
+    axs3 = _Plot_Heatmap_from_DF(con_mat_norm_df, title="Confusion Matrix Normalized", ax = axs3, Accuracy=Accuracy, cohenKappaScore=cohenKappaScore) 
+    #axs3[1] = _Plot_Heatmap_from_DF(con_mat_df, title="Confusion Matrix", ax = axs3[1], Accuracy=Accuracy, cohenKappaScore=cohenKappaScore) 
+    #fig3.tight_layout()
     allMetrics = [prefMetrics, confInterval, auc_roc_vals]
-    return allMetrics, plots
+    if showPlot:
+        plots =[roc_plt, prc_plt,axs2[0], axs2[1], axs2[2], axs3[0],axs3[1] ]
+    else:
+        plots =[fig2,fig, fig3]
+    if printStat==True:
+        print(dfSpec)
+        print(dfAcc)
+        print(confInterval)
+
+    with PdfPages(pdfName+'_report.pdf') as pdf:
+        for x in plots:
+            pdf.savefig(x)
+
+    if returnPlot:
+        return allMetrics, plots
+
+
+def render_df_as_table(data, title = 'Table', col_width=3.0, row_height=0.625, font_size=18,
+                    header_color='#655EE5', row_colors=['#f1f1f2', 'w'], edge_color='w',
+                    bbox=[0, 0, 1, 1], header_columns=0, resetIndex=False,
+                    ax=None, **kwargs):
+    if resetIndex:
+        data = data.reset_index()
+    try:
+        data =data.rename(columns={'index': 'CLASSES'})
+    except:
+        v = None
+    if ax is None:
+        size = (np.array(data.shape[::-1]) + np.array([0, 1])) * np.array([col_width, row_height])
+        fig, ax = plt.subplots(figsize=size)
+        ax.axis('off')
+        plt.title(title, fontsize = font_size+2)
+    else:
+        ax.axis('off')
+        ax.set_title(title, fontsize = font_size+2)
+    df_table = ax.table(cellText=data.values, bbox=bbox, colLabels=data.columns, **kwargs)
+
+    df_table.auto_set_font_size(False)
+    df_table.set_fontsize(font_size)
+
+    for k, cell in  six.iteritems(df_table._cells):
+        cell.set_edgecolor(edge_color)
+        if k[0] == 0 or k[1] < header_columns:
+            cell.set_text_props(weight='bold', color='w')
+            cell.set_facecolor(header_color)
+        else:
+            cell.set_facecolor(row_colors[k[0]%len(row_colors) ])
+    return ax
+
+from sklearn.metrics import accuracy_score,classification_report,cohen_kappa_score
+
+def get_accuracy_score(test_labels, test_predictions):
+	return accuracy_score(test_labels, test_predictions)
+
+def classify_report(y_true,y_pred):
+	return(classification_report(y_true, y_pred, digits=3))
+
+def print_classification_report(y_true,y_pred):
+	print(classification_report(y_true, y_pred, digits=3))
+
+def print_cohen_kappa_score(y_true,y_pred):
+	print(cohen_kappa_score(y_true, y_pred, digits=3))
+
+def _CM_calculate(predictions,ground_truth,classNames):
+    con_mat = confusion_matrix(ground_truth,predictions)
+    con_mat_norm = np.around(con_mat.astype('float') / con_mat.sum(axis=1)[:, np.newaxis], decimals=2)
+    cohenKappaScore = cohen_kappa_score(ground_truth,predictions)
+    Accuracy =float(get_accuracy_score(ground_truth, predictions)*100)
+    con_mat_norm_df = pd.DataFrame(con_mat_norm,
+                                index = classNames, 
+                                columns = classNames)
+    con_mat_df = pd.DataFrame(con_mat,
+                                index = classNames, 
+                                columns = classNames)
+    return con_mat_norm_df, con_mat_df,Accuracy,cohenKappaScore
+
+def _Plot_Heatmap_from_DF(pdDf, title="Confusion Matrix", ax = None, Accuracy=None, cohenKappaScore=None, printTitle=False):
+    subTitleStr= '➤ Model Accuracy:{:.2f}% '.format(Accuracy) if Accuracy is not None else ""
+    subTitleStr+='➤ Cohen Kappa Score {:.3f}'.format(cohenKappaScore) if cohenKappaScore is not None else ""
+    if ax is None:
+        fig,ax = plt.figure(figsize=(10, 10))
+    sns.heatmap(pdDf, annot=True, cmap=plt.get_cmap('PuRd') , ax =ax)
+    if printTitle:
+        ax.set_title("AI-Bharata MedicalAI\n\n", loc='center', fontsize=18,color='grey')
+        ax.set_title('{:}\n{}'.format(title,subTitleStr),loc='left', fontsize=13, )
+    else:
+        ax.set_title("{:}\n\n".format(title), loc='center', fontsize=18,color='grey')
+        ax.set_title('{}'.format(subTitleStr),loc='left', fontsize=13, )
+    ax.set_ylabel('True label',fontsize=18)
+    ax.set_xlabel('Predicted label',fontsize=18)
+    return ax
+
+
+def plot_confusion_matrix(model=None, test_data=None, test_labels =None, labelNames=None, title='Confusion Matrix', predictions=None, showPlot=True, returnPlot=False):
+	if predictions is None:
+		test_predictions = np.argmax(model.predict(test_data), axis=-1)
+	else:
+		test_predictions = np.argmax(predictions, axis=-1)
+	#print(classify_report(test_labels,test_predictions))
+	cohenKappaScore = cohen_kappa_score(test_labels,test_predictions)
+	#print('Cohen kappa Score:', cohenKappaScore)
+	con_mat = confusion_matrix(test_labels,test_predictions)
+	con_mat_norm = np.around(con_mat.astype('float') / con_mat.sum(axis=1)[:, np.newaxis], decimals=2)
+	
+	con_mat_norm_df = pd.DataFrame(con_mat_norm,
+					 index = labelNames, 
+					 columns = labelNames)
+
+	figure = plt.figure(figsize=(10, 10))
+	sns.heatmap(con_mat_df, annot=True, cmap=plt.get_cmap('PuRd') )
+
+	plt.title("AI-Bharata MedicalAI\n\n", loc='center', fontsize=18,color='grey')
+	
+	plt.title('{:}\n➤ Model Accuracy:{:.2f}% ➤ Cohen Kappa Score {:.3f}'.format(title,float(get_accuracy_score(test_labels, test_predictions)*100),cohenKappaScore),loc='left', fontsize=13, )
+	plt.ylabel('True label',fontsize=18)
+	plt.xlabel('Predicted label',fontsize=18)
+	if showPlot:
+		plt.show()
+	if returnPlot:
+		return test_predictions,figure
+	else:
+		return test_predictions
